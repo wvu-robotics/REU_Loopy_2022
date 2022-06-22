9ec401b197fe3f0fc37f3d9cbb80f3bbf22d38c9
coding-nate123 <100448864+coding-nate123@users.noreply.github.com>
2022-06-22 14:34:04 -0400

    Add files via upload

---
+++ loopy_runner.py
@@ -0,0 +1,27 @@
+from re import T
+from time import sleep
+from Loopy import *
+
+NUMBER_OF_AGENTS = 36
+
+
+loopy = Loopy(NUMBER_OF_AGENTS)
+
+
+def main():
+
+    loopy.store_current_shape("testing")
+
+    sleep(5)
+
+    loopy.torque_on_all_agents()
+    loopy.recreate_saved_shape("testing")
+    loopy.torque_off_all_agents()
+
+
+def reset():
+
+    loopy.torque_off_all_agents()
+
+
+main()
