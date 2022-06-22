22be83a8a27bf71e426dd0bd21d3e54c6d5a73c0
saldermann <107275808+saldermann@users.noreply.github.com>
2022-06-22 09:13:50 -0400

    Add files via upload

---
+++ GUI/Agent.py
@@ -0,0 +1,24 @@
+import random
+
+def TourqeState(Agent):
+    Agent.state = int(random.randrange(-1,100)) ##change to actual range later - 0 low/off grey, 1 med/good green, 2 high/danger red
+
+def GoalAngle(Agent):
+    Agent.goal = int(random.randrange(0,359))
+
+def CurrentAngle(Agent):
+    Agent.angle = int(random.randrange(0,359))
+
+def Position(Agent):
+    Agent.pose = [random.randint(1,9),random.randint(1,9)]
+
+class Agent:
+    def __init__(self, name, state, goal, angle, pose):
+        self.name = name
+        self.state = state      #tourque (#s, high med low)
+        self.goal = goal      #goal angle
+        self.angle = angle      #current angle
+        self.pose = pose        #current position (x,y)
+
+
+
